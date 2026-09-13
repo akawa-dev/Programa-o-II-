135647985f83259ca3a9654f0e2611623f147d82
akawa <akawa.dev@gmail.com>
2026-09-13 20:21:24 -0300

    atualizando codigo

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,26 @@
 class Carro {
-    String cor;
     String modelo;
+    String cor;
     double velocidade;
 
     void acelerar() {
         velocidade += 10;
     }
 }
-
 class CarroEsportivo extends Carro {
     void ativarTurbo() {
         velocidade += 50;
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         CarroEsportivo c = new CarroEsportivo();
+        c.modelo = "Uno com escada";
         c.cor = "vermelho";
-        c.modelo = "Fusca Turbo";
         c.velocidade = 0;
 
         c.acelerar();
         c.ativarTurbo();
-
         System.out.println(c.velocidade);
     }
 }
